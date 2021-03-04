628055be14aa98abfcf2cd2657d51b3d7bf622e5
Junior Perondi <junior@ponteon.com.br>
2021-03-04 10:13:11 -0300

    get url parameter and send in conversion

---
+++ js/parmstoform.js
@@ -0,0 +1,37 @@
+interval = null;
+function findGetParameter(parameterName) {
+    var result = "",tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    }
+    return result;
+}
+function a2forms() {
+    skipThis = false;
+    var formsCollection = document.getElementsByTagName("form");
+    for(var i=0;i<formsCollection.length;i++) {
+        formulario = formsCollection[i];   
+        for (var j = 0; j < formulario.elements.length; j++) {
+            if (formulario.elements.item(j).type.toLowerCase() == "hidden" && formulario.elements.item(j).name.toLowerCase() == "a") {
+                skipThis = true;
+                break;
+            }
+        }
+        if(!skipThis) {
+            var input = document.createElement("input");
+            input.setAttribute("type", "hidden");
+            input.setAttribute("name", "id_afiliado_eduzz");
+            input.setAttribute("value", findGetParameter("a"));
+            formulario.appendChild(input);
+        }             
+    }
+    clearInterval(this.interval);
+    this.interval = setInterval(function () { 
+                                    a2forms();
+                                }, 3000);
+}
+if(findGetParameter("utm_source")!="") {
+    a2forms();    
+}
